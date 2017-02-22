97d6d34b420cdbc449e2cee0e61fcb07c695f47e
fatfisz <fatfisz@gmail.com>
2017-02-23 00:05:45 +0100

    Create the website

---
+++ pages/_document.js
@@ -0,0 +1,37 @@
+import Document, { Head, Main, NextScript } from 'next/document';
+
+
+export default class extends Document {
+  render () {
+    return (
+      <html>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        </Head>
+        <style jsx global>{`
+          html, body, #__next, [data-reactroot] {
+            height: 100%;
+          }
+
+          html {
+            font-family: sans-serif;
+          }
+
+          body {
+            color: #212121;
+            margin: 0;
+          }
+
+          p {
+            margin: 1rem 0;
+          }
+        `}</style>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
---
+++ pages/index.js
@@ -0,0 +1,45 @@
+import Head from 'next/head';
+
+
+export default function Index() {
+  return (
+    <div className="main-container">
+      <Head>
+        <title>FatFisz's humble site</title>
+      </Head>
+
+      <main className="main">
+        <h2>
+          Hi, I'm Rafał.
+        </h2>
+        <p>
+          I once had many talents. Now I build websites.
+        </p>
+        <p>
+          I'm currently trying to make <a href="https://codility.com/">Codility</a> more awesome.
+        </p>
+        <p>
+          I have some projects on GitHub: <a href="https://github.com/fatfisz">@fatfisz</a>
+        </p>
+        <br />
+        <p>
+          More content coming soon.
+        </p>
+      </main>
+
+      <style jsx>{`
+        .main-container {
+          align-items: center;
+          display: flex;
+          flex-direction: column;
+          height: 100%;
+          justify-content: center;
+        }
+
+        .main {
+          flex: 0 0 auto;
+        }
+      `}</style>
+    </div>
+  );
+}
