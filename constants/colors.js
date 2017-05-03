2a61c513f0c84613bc5a5b778667c2329b024588
fatfisz <fatfisz@gmail.com>
2017-05-03 18:56:24 +0200

    Add color constants

---
+++ constants/colors.js
@@ -0,0 +1,7 @@
+export const lightBlue700 = '#0288d1';
+export const lightBlue800 = '#0277bd';
+export const lightBlue900 = '#01579B';
+export const amber200 = '#ffe082';
+export const amber300 = '#ffd54f';
+export const grey800 = '#424242';
+export const grey900 = '#212121';
