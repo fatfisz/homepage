0cea517df4c44821723b18a7c63f2d96cb0d85b8
fatfisz <fatfisz@gmail.com>
2017-06-25 23:33:06 +0200

    Add webpack-bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
 'use strict';
 
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+
 module.exports = {
   onDemandEntries: {
     maxInactiveAge: Infinity,
+  },
+
+  webpack(config) {
+    config.plugins.push(
+      new BundleAnalyzerPlugin({
+        analyzerMode: 'disabled',
+        generateStatsFile: true,
+        statsFilename: 'stats.json',
+      })
+    );
+
+    return config;
   },
 };
