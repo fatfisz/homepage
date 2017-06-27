8f9dbcd4678e981a1357f334f2adef68c2a4560d
fatfisz <fatfisz@gmail.com>
2017-06-27 10:33:35 +0200

    Remove the property attribute from og meta tags

--- components/body/index.js
+++ components/body/index.js
@@ -15,9 +15,9 @@ export default function Body({ children }) {
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:400,700" />
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@fatfisz" />
-        <meta name="og:type" property="og:type" content="website" />
-        <meta name="og:image" property="og:image" content="https://www.gravatar.com/avatar/dcc938049e3618d715d4e45eeb2cc314" />
-        <meta name="og:description" property="og:description" content="FatFisz's humble homepage." />
+        <meta name="og:type" content="website" />
+        <meta name="og:image" content="https://www.gravatar.com/avatar/dcc938049e3618d715d4e45eeb2cc314" />
+        <meta name="og:description" content="FatFisz's humble homepage." />
       </Head>
 
       <GlobalStyles />
