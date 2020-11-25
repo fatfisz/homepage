c176c32eaabcd9527ee336e7bfaf9bef4247cae5
fatfisz <fatfisz@gmail.com>
2020-11-25 20:32:44 +0100

    Who needs comments? Not me

--- components/post.js
+++ components/post.js
@@ -1,7 +1,5 @@
 import Body from 'components/body';
 import CodeExampleStyles from 'components/code-example-styles';
-import Disqus from 'components/disqus';
-import DisqusLink from 'components/disqus-link';
 import Title from 'components/title';
 import Head from 'next/head';
 import PropTypes from 'prop-types';
@@ -20,15 +18,12 @@ export default function Post({ body, date, excerpt, id, title }) {
 
       <h5 className="date-header">
         <div className="date">{date}</div>
-        <DisqusLink href={href} id={id} />
       </h5>
 
       <h2>{title}</h2>
 
       <CodeExampleStyles />
       {reactFromObject(JSON.parse(body))}
-
-      <Disqus url={href} id={id} />
 
       <style jsx>{`
         .date-header {
